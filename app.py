35958377e1aec828e44e045255beb09e08dcb9ec
stagiairedev4 <164581937+stagiairedev4@users.noreply.github.com>
2024-03-27 15:55:09 +0100

    Update app.py
    2.0

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+e from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World ! - Pyflask Demo</h1>'
+    return '<h1>Hello, World ! - Pyflask mon pipeline à moi moi moi moi</h1>'
 
 @app.route('/version')
 def get_version():
-    return '<h1>App version : <b>1.0</b></h1>'
+    return '<h1>App version : <b>2.0</b></h1>'
 
 @app.route('/test')
 def get_test():
